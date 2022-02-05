d74de3e3da1dcb5a7a17043772cb1d397afd6cc2
anthonypoon12 <anthony@poons.us>
2022-02-05 17:11:18 -0500

    Added database functionality with register
    You can now use the login page as a register page. It will upload the
    username and password into a database. It will then direct you to the
    main Project page.

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:password@localhost/login'
+db=SQLAlchemy(app)
+class Data(db.Model):
+    __tablename__="data"
+    id=db.Column(db.Integer,primary_key=True)
+    email_=db.Column(db.String(120),unique=True)
+    password_=db.Column(db.String(120))
+
+    def __init__(self, email_, password_):
+        self.email_=email_
+        self.password_=password_
+@app.route("/")
+def login():
+    return render_template("login.html")
+@app.route("/Project5",methods=['POST'])
+def Project5():
+    if request.method=='POST':
+        email=request.form["email"]
+        password=request.form["password"]
+        print(request.form)
+        data=Data(email,password)
+        db.session.add(data)
+        db.session.commit()
+        return render_template("Project5.html")
+if __name__ == '__main__':
+    app.debug=True
+    app.run()
