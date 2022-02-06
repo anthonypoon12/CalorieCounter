8db3b02a97baeb62ea290a71eaa532198e77f14f
Jhwee2 <jared.hwee@baruchmail.cuny.edu>
2022-02-06 12:47:15 -0500

    Newest Version of file

--- static/Project.js
+++ static/Project.js
@@ -14,26 +14,31 @@ async function search(name) {
 }
 
 function gotData(data) {
-  fName = data.results[0].title
-  servings = data.results[0].servings
-  fCalories = data.results[0].nutrition.nutrients[0].amount
-  fCalories = (parseFloat(fCalories)/parseFloat(servings)).toFixed(1)
-  fCalories = String(fCalories)
-  console.log(fName, servings, fCalories);
-  $("#table").append(
-    "<tr class=\"text-center\">" +
-    "<th scope=\"row\">" + count + "</th>" +
-    "<td class=\"fw-bold\">" + fName + "</td>" +
-    "<td> <input id="+count+"\" class=\"form-control\"placeholder=\"Enter Number of Servings:\" onkeyup=\"getValue(this)\"></td>" +
-    "<td class=\"fw-bold calorie\">" + fCalories + "</td>" +
-    "</tr>");    
-    let todayTotalCalories = 0
-    for (let i = 1; i < table.rows.length; i++) {
-      todayTotalCalories = todayTotalCalories + parseInt(table.rows[i].cells[3].innerHTML)
+  if(data.results.length>0){
+    fName = data.results[0].title
+    servings = data.results[0].servings
+    fCalories = data.results[0].nutrition.nutrients[0].amount
+    fCalories = (parseFloat(fCalories)/parseFloat(servings)).toFixed(1)
+    fCalories = String(fCalories)
+    console.log(fName, servings, fCalories);
+    count++
+    $("#table").append(
+      "<tr class=\"text-center\">" +
+      "<th scope=\"row\">" + count + "</th>" +
+      "<td class=\"fw-bold\">" + fName + "</td>" +
+      "<td> <input id="+count+"\" class=\"form-control\"placeholder=\"Enter Number of Servings:\" onkeyup=\"getValue(this)\"></td>" +
+      "<td class=\"fw-bold calorie\">" + fCalories + "</td>" +
+      "</tr>");    
+      let todayTotalCalories = 0
+      for (let i = 1; i < table.rows.length; i++) {
+        todayTotalCalories = todayTotalCalories + parseInt(table.rows[i].cells[3].innerHTML)
+      }
+      $("#totalCalories").text(todayTotalCalories)
     }
-    $("#totalCalories").text(todayTotalCalories)
+    else {
+      alert("Try Again!");
+    }
 }
-
 
 
 let count = 0
@@ -49,7 +54,6 @@ $("#btn1").click(function () {
     alert(food + " is not a valid input")
   } else {
     food = search(food)
-    count++
 
   }
 })
